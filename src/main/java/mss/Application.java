5981eef3e079c5e9c376b5f9d76ba97f311dc069
Leonard Krause <leoek2011@gmail.com>
2018-05-29 03:39:29 +0200

    initial gradle project

---
+++ src/main/java/mss/Application.java
@@ -0,0 +1,24 @@
+package mss;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        log.info("Done");
+        System.exit(0);
+    }
+}
