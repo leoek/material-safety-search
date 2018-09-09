f95395078db3bc5ed32c73aa5a0d5db4fdf5710c
Leonard Krause <leoek2011@gmail.com>
2018-09-09 17:54:50 +0200

    add solrj example usage, remove solr dependency (it is included in sprin-data-solr) and removing it avoids dependency issues

--- src/main/java/mss/Application.java
+++ src/main/java/mss/Application.java
@@ -1,13 +1,26 @@
 package mss;
 
+import mss.domain.entity.DataSheetDocument;
 import mss.domain.repository.DataSheetRepository;
 import mss.domain.service.DataSheetImporter;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -24,20 +37,39 @@ public class Application implements CommandLineRunner {
     @Autowired
     private DataSheetImporter dataSheetImporter;
 
-    @Override
-    public void run(String... strings) throws Exception {
-
-        /*if (dataSheetRepository.count() == 0){
-            dataSheetImporter.importDataSet();
-            log.info("import done");
-        }
-
+    public void examples() {
         Pageable pageable = PageRequest.of(0, 10);
 
         Page<DataSheetDocument> documents = dataSheetRepository.findFullText("SEMI GLOSS INTERIOR LATEX WHITE", pageable);
 
         log.info("{}", documents);
 
-        System.exit(0);*/
+        String urlString = "http://localhost:8983/solr/dataSheet";
+        SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+
+        SolrQuery query = new SolrQuery();
+        query.setQuery("lines:oxygen");
+        query.setStart(0);
+
+        try {
+            QueryResponse response = solr.query(query);
+            SolrDocumentList results = response.getResults();
+            log.info("{}", results);
+        } catch (SolrServerException | IOException solrE){
+            log.info("{}", solrE);
+        }
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        if (dataSheetRepository.count() == 0){
+            dataSheetImporter.importDataSet();
+            log.info("import done");
+        }
+
+        examples();
+
+        //System.exit(0);
     }
 }
