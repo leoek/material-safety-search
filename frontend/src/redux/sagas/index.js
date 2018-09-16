e253c6662a1e958815efdeb92bd486212d624d22
Leonard Krause <leoek2011@gmail.com>
2018-09-16 18:03:33 +0200

    #8 add error handling for the json prsing

--- frontend/src/redux/sagas/index.js
+++ frontend/src/redux/sagas/index.js
@@ -3,7 +3,8 @@ import { REHYDRATE } from "redux-persist";
 import {
   reduxRehydrationCompleted,
   FETCH_SEARCH_REQUEST,
-  fetchSearchSuccess
+  fetchSearchSuccess,
+  fetchSearchFailure
 } from "../actions";
 import { get } from "../../lib/api";
 import { config } from "../../config";
@@ -11,6 +12,21 @@ import { config } from "../../config";
 export function* reduxRehydrateSaga(action) {
   yield put(reduxRehydrationCompleted());
 }
+
+/**
+ * redux-saga expects that a yielded promise will be resolved,
+ * therefore we catch any parsing errors here, create our custom
+ * error object and resolve the promise anyways.
+ * We cannot let the app crash because of a malformed response.
+ */
+const handleResponseJsonError = (errorMessage, statusCode) => {
+  return new Promise(resolve => {
+    const error = {
+      error: config.ERROR.UNPARSABLE_RESPONSE
+    };
+    resolve(error);
+  });
+};
 
 export function* fetchSearchSaga(action) {
   const { payload = {} } = action;
@@ -24,8 +40,12 @@ export function* fetchSearchSaga(action) {
     endpoint: "search",
     parameters
   });
-  const data = yield response.json();
-  yield put(fetchSearchSuccess(data));
+  const data = yield response.json().catch(handleResponseJsonError);
+  if (response.ok) {
+    yield put(fetchSearchSuccess(data));
+  } else {
+    yield put(fetchSearchFailure(data));
+  }
 }
 
 export default function* root() {
