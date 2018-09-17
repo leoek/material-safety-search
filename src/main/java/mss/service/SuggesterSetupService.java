1891d29fc800cd7489f8ea4d023cffb48ef06e92
fxj9a <adrian.sprucer@gmail.com>
2018-09-17 22:08:47 +0200

    SuggesterSetupService for solrconfig

---
+++ src/main/java/mss/service/SuggesterSetupService.java
@@ -0,0 +1,42 @@
+package mss.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.catalina.LifecycleState;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.GenericSolrRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.SimpleSolrResponse;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.util.ContentStream;
+import org.apache.solr.common.util.ContentStreamBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class SuggesterSetupService {
+
+    private static final Logger log = LoggerFactory.getLogger(SuggesterSetupService);
+
+    @Autowired
+    private SolrClient dssc; // data sheet solr client
+
+    public void setup() throws IOException, SolrServerException {
+
+
+
+    }
+
+}
