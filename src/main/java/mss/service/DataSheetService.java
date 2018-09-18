7ec8fb8a8edfb40413062afc7f97e96a251ce6bc
fxj9a <adrian.sprucer@gmail.com>
2018-09-18 16:17:09 +0200

    Response to REST get request

--- src/main/java/mss/service/DataSheetService.java
+++ src/main/java/mss/service/DataSheetService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.solr.core.query.result.FacetPage;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -26,4 +27,9 @@ public class DataSheetService {
         log.info(result.toString());
         return result;
     }
+/*
+    public FacetPage<DataSheetService> framentedFullText (Pageable p, String searchTerm) {
+        FacetPage<DataSheetService> result = dataSheetRepository.findAll(Pageable p, searchTerm);
+    }
+    */
 }
--- src/main/java/mss/service/SuggestService.java
+++ src/main/java/mss/service/SuggestService.java
@@ -3,35 +3,52 @@ package mss.service;
 
 import mss.domain.entity.DataSheetDocument;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.solr.core.query.result.FacetFieldEntry;
 import org.springframework.data.solr.core.query.result.FacetPage;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-
+@RestController
+@Component
 public class SuggestService {
 
+    private static final Logger log = LoggerFactory.getLogger(DataSheetService.class);
+
+    private DataSheetService dataSheetService;
+
+    @Autowired
+    public SuggestService(DataSheetService dataSheetService){
+        this.dataSheetService = dataSheetService;
+    }
+
+
     @ResponseBody
-    @RequestMapping(value="/autocomplete", produces = "application/json")
-    public Set<String> autocomplete(Model model, @RequestParam("term") String query) {
+    @GetMapping
+    @RequestMapping(path="/autocomplete", produces = "application/json")
+    public Set<String> autocomplete(@RequestParam("term") String query) {
         if (StringUtils.isBlank(query)) {
             return Collections.emptySet();
         }
 
         PageRequest pageRequest = PageRequest.of(0, 1);
-        FacetPage<DataSheetDocument> result = DataSheetService.findFullText(pageRequest, query);
+
+        FacetPage<DataSheetDocument> result = (FacetPage<DataSheetDocument>) dataSheetService.findFullText(pageRequest, query);
+
 
         Set<String> suggestions = new LinkedHashSet<String>();
         for (Page<FacetFieldEntry> page : result.getFacetResultPages()) {
+            log.info(page.toString());
             for (FacetFieldEntry entry : page) {
+                log.info(entry.toString());
                 if (entry.getValue().contains(query)) {
                     suggestions.add(entry.getValue());
                 }
