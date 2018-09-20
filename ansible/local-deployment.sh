66138ab0dc37936893b82c2421bb78f3f78947b2
Leonard Krause <leoek2011@gmail.com>
2018-09-20 21:55:17 +0200

    handle docker login and copy solr.in.sh scrip

--- ansible/local-deployment.sh
+++ ansible/local-deployment.sh
@@ -6,6 +6,14 @@ echo "net1" >> hosts-deployment
 
 cat hosts-deployment
 
+DOCKER_PW_TO_REPLACE="MSS_DEPLOY_DOCKER_HUB_PW_TO_BE_REPLACE"
+dockerPw_esc=$(echo "$dockerPw" | sed -e 's/[\/&]/\\&/g');
+sed -i -e "s/$DOCKER_PW_TO_REPLACE/$dockerPw_esc/g" roles/docker-mss-deployment-local/defaults/main.yml
+
+DOCKER_USER_TO_REPLACE="MSS_DEPLOY_DOCKER_HUB_USER_TO_BE_REPLACE"
+dockerUser_esc=$(echo "$dockerUser" | sed -e 's/[\/&]/\\&/g');
+sed -i -e "s/$DOCKER_USER_TO_REPLACE/$dockerUser_esc/g" roles/docker-mss-deployment-local/defaults/main.yml
+
 docker pull leoek/ansible:2.4-did
 docker run --rm -v $(pwd):/ansible/playbooks \
 	-v /srv/docker/swt:/srv/docker/swt \
