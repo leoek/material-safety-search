bd7e8e563c2421e78767ccf8a7a5cb29bbc12c70
Leonard Krause <leoek2011@gmail.com>
2018-09-20 21:01:46 +0200

    add ansible for auto deployments to a staging server

---
+++ ansible/local-deployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ip=$(docker network inspect --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}' nginx-proxy | awk -F '/' 'NR==1{print $1}')
+echo "net1 ansible_host=$ip" > hosts-swt-deployment
+echo "[mss]" >> hosts-deployment
+echo "net1" >> hosts-deployment
+
+cat hosts-deployment
+
+docker pull leoek/ansible:2.4-did
+docker run --rm -v $(pwd):/ansible/playbooks \
+	-v /srv/docker/swt:/srv/docker/swt \
+    -v  /var/run/docker.sock:/var/run/docker.sock:ro \
+    leoek/ansible:next playbook-deployment.yml -i hosts-mss-deployment --connection=local
