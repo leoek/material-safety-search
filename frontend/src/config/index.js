a1f44b3e868fc17d3375a40fde2f6249522e0289
Leonard Krause <leoek2011@gmail.com>
2018-09-20 14:45:12 +0200

    use api base url from the environment in frontend production builds

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,9 +1,12 @@
-export const config = {
+const devEnv = process.env.NODE_ENV === "development";
+
+const devConfig = {
+  apiBaseUrl: "http://localhost:8080"
+};
+
+export const baseConfig = {
   version: "0.1.0",
-  apiBaseUrl:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:8080"
-      : "https://api.mss.leoek.tech",
+  apiBaseUrl: process.env.API_BASE_URL || "https://api.mss.leoek.tech",
   ERROR: {
     NOCONNECTION: "NOCONNECTION",
     UNPARSABLE_RESPONSE: "UNPARSABLE_RESPONSE"
@@ -11,6 +14,12 @@ export const config = {
   DEFAULTS: {
     pageSize: 10
   }
+};
+
+export const config = {
+  ...baseConfig,
+  ...(devEnv ? devConfig : {}),
+  devEnv
 };
 
 export default config;
