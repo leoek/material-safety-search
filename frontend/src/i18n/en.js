a998e394d7864f2aedb54f1461236512d35a3099
Leonard Krause <leoek2011@gmail.com>
2018-09-22 22:57:18 +0200

    add translations

--- frontend/src/i18n/en.js
+++ frontend/src/i18n/en.js
@@ -10,6 +10,14 @@ export default {
         submit: "Search",
         querylbl: "Search"
       }
+    },
+    datasheet: {
+      niin: "NIIN",
+      fsc: "FSC",
+      fsg: "FSG",
+      FirstAid: "First Aid Instructions",
+      FireFighting: "Firefighting Instructions"
     }
-  }
+  },
+  show_more: "Show More"
 };
