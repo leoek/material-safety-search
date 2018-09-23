0edf053148f8d24e466661c5bfe9ef9d491a5a03
David Reinartz <d.c.reinartz@gmail.com>
2018-09-23 19:09:57 +0200

    Add query for top terms to Logging Repository

--- src/main/java/mss/domain/repository/LogRepository.java
+++ src/main/java/mss/domain/repository/LogRepository.java
@@ -1,8 +1,12 @@
 package mss.domain.repository;
 
 import mss.domain.entity.Log;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LogRepository extends JpaRepository<Log, Long> {
-    
+    @Query(value = "select t.searchTerm, count(t.searchTerm) as occurance from mariadb.log as t group by t.searchTerm order by occurance desc")
+    public Page<String> getAll(Pageable p);
 }
