78feec2874896beff7636fbdf55a1490e0338df4
Leonard Krause <leoek2011@gmail.com>
2018-11-05 01:40:03 +0100

    add ansible role to deploy multiple frontend images

--- ansible/local-deployment.sh
+++ ansible/local-deployment.sh
@@ -4,6 +4,20 @@ ansible_role="playbook-deployment.yml"
 if [ "$1" == "reset-volumes" ]; then
     ansible_role="playbook-deployment-reset-volumes.yml"
 fi
+if [ "$1" == "only-frontend" ]; then
+    . ./gitvars.sh
+
+    MSS_PATH_TO_REPLACE="mss_path: /srv/docker/mssfrontend"
+    MSS_PATH_TO_REPLACE_esc=$(echo "$MSS_PATH_TO_REPLACE" | sed -e 's/[\/&]/\\&/g');
+    mssPath="mss_path: /srv/docker/mssfrontend-$git_esc"
+    mssPath_esc=$(echo "$mssPath" | sed -e 's/[\/&]/\\&/g');
+    sed -i -e "s/$MSS_PATH_TO_REPLACE_esc/$mssPath_esc/g" roles/docker-mss-deployfrontend-local/defaults/main.yml
+    DOMAIN_PREFIX_TO_REPLACE="mss_domain_prefix: frontend"
+    DOMAIN_PREFIX_TO_REPLACE_esc=$(echo "$DOMAIN_PREFIX_TO_REPLACE" | sed -e 's/[\/&]/\\&/g');
+    mssDomainPrefix_esc=$(echo "$git_esc" | sed -e 's/[\/&]/\\&/g');
+    sed -i -e "s/$DOMAIN_PREFIX_TO_REPLACE_esc/$mssDomainPrefix_esc/g" roles/docker-mss-deployfrontend-local/defaults/main.yml
+    ansible_role="playbook-deployfrontend.yml"
+fi
 
 ip=$(docker network inspect --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}' nginx-proxy | awk -F '/' 'NR==1{print $1}')
 echo "net1 ansible_host=$ip" > hosts-deployment
